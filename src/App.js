9c30bafb51013b1b8318d7b75002f7948f467023
Akhil Kumar E <eeturiakhil@gmail.com>
2019-12-25 12:02:37 +0530

    Finished Todo list React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+
+  addItem(todoValue) {
+    if (todoValue != "") {
+      const newItem = {
+        id: Date.now(),
+        value: todoValue,
+        isDone: false
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItem: ""
+      });
+    }
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({ list: updatedList });
+  }
+
+  updateInput(input) {
+    this.setState({ newItem: input });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {/* <img src={logo} className="App-logo" /> */}
+          <h1>This is React Crash Project </h1>
+        </div>
+        <div>
+          Add an Item...
+          <br />
+          <input
+            type="text"
+            placeholder="Add a ToDo"
+            value={this.state.newItem}
+            onChange={e => this.updateInput(e.target.value)}
+            required
+          />
+          <button
+            onClick={() => this.addItem(this.state.newItem)}
+            disabled={!this.state.newItem.length}
+          >
+            Add Todo
+          </button>
+        </div>
+        <div>
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button onClick={() => this.deleteItem(item.id)}>
+                    Delete
+                  </button>
+                </li>
+              );
+            })}
+            <li>Added Todo list</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
